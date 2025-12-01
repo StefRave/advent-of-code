a5fbe0c848de6347e8bd2aa8fb0f4f5790687737
Stef Rave <stef.rave@gmail.com>
2025-12-01 09:56:43 +0100

    2025 Day01

--- 2025/Day01.cs
+++ 2025/Day01.cs
@@ -4,10 +4,33 @@ public class Day01 : IAdvent
 {
     public void Run()
     {
-        int answer1 = 0;
-        Advent.AssertAnswer1(answer1, expected: 1, sampleExpected: 11);
+        var input = Advent.ReadInput().SplitByNewLine()
+            .Select(x => new { Rot = x[0], Times = int.Parse(x[1..]) });
 
+        int pos = 50;
+        int answer1 = 0;
         int answer2 = 0;
-        Advent.AssertAnswer2(answer2, expected: 2, sampleExpected: 22);
+        foreach (var item in input)
+        {
+            for (int i = 0; i < item.Times; i++)
+            {
+                if (item.Rot == 'L')
+                    pos--;
+                else
+                    pos++;
+
+                if (pos < 0)
+                    pos = 99;
+                if (pos == 100)
+                    pos = 0;
+
+                if (pos == 0)
+                    answer2++;
+            }
+            if (pos == 0)
+                answer1++;
+        }
+        Advent.AssertAnswer1(answer1, expected: 1081, sampleExpected: 3);
+        Advent.AssertAnswer2(answer2, expected: 6689, sampleExpected: 6);
     }
 }
