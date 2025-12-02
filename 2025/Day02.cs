8bb94ba477b2c2965df94e814f9fb370b2b1b881
Stef Rave <stef.rave@gmail.com>
2025-12-02 20:05:29 +0100

    2025 Day02

--- 2025/Day02.cs
+++ 2025/Day02.cs
@@ -4,10 +4,42 @@ public class Day02 : IAdvent
 {
     public void Run()
     {
-        int answer1 = 0;
-        Advent.AssertAnswer1(answer1, expected: 1, sampleExpected: 11);
+        var input = Advent.ReadInput().Split(',')
+            .Select(line => line.Split('-').Select(long.Parse).ToArray())
+            .ToArray();
 
-        int answer2 = 0;
-        Advent.AssertAnswer2(answer2, expected: 2, sampleExpected: 22);
+        long answer1 = 0;
+        long answer2 = 0;
+        foreach (var line in input.Skip(0))
+        {
+            for (long i = line[0]; i <= line[1]; i++)
+            {
+                string val = i.ToString();
+                bool found = false;
+                for (int len = 1; len < val.Length; len++)
+                {
+                    if ((val.Length % len) != 0)
+                        continue;
+                    string firstPart = val[0..len];
+                    bool match = true;
+                    for (int j = len; j < val.Length; j += len)
+                        match &= firstPart == val[j..(j + len)];
+                    if (match)
+                    {
+                        if (!found)
+                            answer2 += i;
+                        found = true;
+
+                        if (len * 2 == val.Length)
+                        {
+                            answer1 += i;
+                        }
+                    }
+                }
+            }
+        }
+
+        Advent.AssertAnswer1(answer1, expected: 26255179562, sampleExpected: 1227775554);
+        Advent.AssertAnswer2(answer2, expected: 31680313976, sampleExpected: 4174379265);
     }
 }
