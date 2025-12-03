c08fe63d2af234a13b5cbf0d366308a264ab0770
Stef Rave <stef.rave@gmail.com>
2025-12-03 06:27:26 +0100

    2025 Day03

--- 2025/Day03.cs
+++ 2025/Day03.cs
@@ -4,10 +4,42 @@ public class Day03 : IAdvent
 {
     public void Run()
     {
-        int answer1 = 0;
-        Advent.AssertAnswer1(answer1, expected: 1, sampleExpected: 11);
+        var input = Advent.ReadInputLines()
+            .Select(line => line.Select(c => c - '0').ToArray())
+            .ToArray();
 
-        int answer2 = 0;
-        Advent.AssertAnswer2(answer2, expected: 2, sampleExpected: 22);
+        var answer1 = GetTotalJolts(input, 2);
+        Advent.AssertAnswer1(answer1, expected: 16842, sampleExpected: 357);
+
+        long answer2 = GetTotalJolts(input, 12);
+        Advent.AssertAnswer2(answer2, expected: 167523425665348, sampleExpected: 3121910778619);
+    }
+
+    private static long GetTotalJolts(int[][] input, int digits)
+    {
+        long totalJolts = 0;
+        foreach (var line in input)
+        {
+            long jolts = 0;
+            int maxPos = 0;
+            int maxPrev = -1;
+            for (int digit = digits; digit > 0; digit--)
+            {
+                int max = -1;
+                for (int i = maxPrev + 1; i < line.Length - digit + 1; i++)
+                {
+                    if (line[i] > max)
+                    {
+                        max = line[i];
+                        maxPos = i;
+                    }
+                }
+                maxPrev = maxPos;
+                jolts = jolts * 10 + max;
+            }
+            totalJolts += jolts;
+        }
+
+        return totalJolts;
     }
 }
