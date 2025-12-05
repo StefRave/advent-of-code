0368fd68afbad9206732b29a0fe0e0d13c33bbd0
Stef Rave <stef.rave@gmail.com>
2025-12-05 06:26:17 +0100

    2025 Day05

--- 2025/Day05.cs
+++ 2025/Day05.cs
@@ -4,10 +4,43 @@ public class Day05 : IAdvent
 {
     public void Run()
     {
-        int answer1 = 0;
-        Advent.AssertAnswer1(answer1, expected: 1, sampleExpected: 11);
+        var input = Advent.ReadInput().SplitByDoubleNewLine();
+        var ingredientRanges = input[0].SplitByNewLine()
+            .Select(line => line.Split('-').Select(long.Parse).ToArray())
+            .Select(r => (start: r[0], end: r[1]))
+            .ToArray();
+        var ingredientIds = input[1]
+            .SplitByNewLine()
+            .Select(long.Parse)
+            .ToArray();
 
-        int answer2 = 0;
-        Advent.AssertAnswer2(answer2, expected: 2, sampleExpected: 22);
+        long freshIngredients = 0;
+        foreach (long ingredient in ingredientIds)
+        {
+            foreach (var range in ingredientRanges)
+            {
+                if (ingredient >= range.start && ingredient <= range.end)
+                {
+                    freshIngredients++;
+                    break;
+                }
+            }
+        }
+        Advent.AssertAnswer1(freshIngredients, expected: 726, sampleExpected: 3);
+
+
+        ingredientRanges = ingredientRanges.OrderBy(r => r.start).ToArray();
+        freshIngredients = 0;
+        long pos = 0;
+        foreach (var range in ingredientRanges)
+        {
+            pos = Math.Max(range.start, pos);
+            if (pos > range.end)
+                continue;
+            freshIngredients += range.end - pos + 1;
+            pos = range.end + 1;
+        }
+
+        Advent.AssertAnswer2(freshIngredients, expected: 354226555270043, sampleExpected: 14);
     }
 }
